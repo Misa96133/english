e13b4849f73f6334af23414eb7ba3aacc6a78187
Misa <misa.kuroi.81@gmail.com>
2024-08-20 21:28:49 +0900

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// const hamburger = document.querySelector(".hamburger");
+
+// const navi = document.getElementById("navi");
+
+
+// hamburger.addEventListener("click", function () {
+//     navi.classList.toggle("active");
+// });
+
+// navi.addEventListener("click", function () {
+//     navi.classList.toggle("active");
+// });
+const hamburger = document.querySelector(".hamburger");
+const navi = document.getElementById("navi");
+
+hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("active");
+    navi.classList.toggle("active");
+});
